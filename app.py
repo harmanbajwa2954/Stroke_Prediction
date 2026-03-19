e1abcbb65b19a51a45028faab358c8c1c6b00b38
Harmanpreet Singh <harmanbajwa012005@gmail.com>
2026-03-19 20:48:03 +0530

    Change app port from 5000 to 7860

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def predict():
 
 if __name__ == '__main__':
     # Run the app locally on port 5000
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=7860)
